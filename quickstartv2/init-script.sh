d20e5795bb553c0a6dc6ad39234b5f95c642f80a
Juan Bonilla <jjbonillatroncoso@gmail.com>
2024-01-03 21:44:25 -0500

    update

---
+++ quickstartv2/init-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the directory where the repo will reside
+REPO_DIR="/meltano"
+
+# Check if the directory exists
+if [ -d "$REPO_DIR" ]; then
+    # If it exists, update the repo
+    cd "$REPO_DIR"
+    git pull
+else
+    # If it doesn't exist, clone the repo
+    git clone https://github.com/JJBon/meltano_demos.git "$REPO_DIR"
+fi
+
+# Navigate to the Meltano project directory and start Meltano
+cd "$REPO_DIR"/quickstartv2/demo-project
+# Add commands to start Meltano or other necessary services
+meltano run tap-
